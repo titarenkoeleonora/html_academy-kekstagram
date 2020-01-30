6586f0475cc220fcb4e6569c4cf41526143eb82d
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-01-30 22:22:05 +0300

    Задание 3

---
+++ js/main.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var PICTURE_COUNT = 25;
+var USER_NAMES = ['Полина', 'Маша', 'Даша', 'Олег', 'Дима', 'Кристина', 'Катя', 'Саша'];
+var MESSAGE_TEXT = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+
+var getRandomElement = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var getRandomIntInclusive = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var shuffle = function (arr) {
+  var j = 0;
+  var x = 0;
+
+  for (var i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = arr[i];
+    arr[i] = arr[j];
+    arr[j] = x;
+  }
+  return arr;
+};
+
+var createComment = function (messageText) {
+  var commentsArr = shuffle(messageText).slice(getRandomIntInclusive(0, messageText.length));
+  var comments = [];
+  for (var i = 0; i <= commentsArr.length; i++) {
+    comments.push({
+      avatar: 'img/avatar' + getRandomIntInclusive(1, 6) + '.svg',
+      message: getRandomElement(messageText),
+      name: getRandomElement(USER_NAMES)
+    });
+  }
+
+  return comments;
+};
+
+var picturesArray = function (count) {
+  var photos = [];
+  for (var i = 0; i < count; i++) {
+    photos.push({
+      url: 'photos/' + (i + 1) + '.jpg',
+      description: 'Описание фотографии',
+      likes: getRandomIntInclusive(15, 200),
+      comments: createComment(MESSAGE_TEXT)
+    });
+  }
+  return photos;
+};
+
+var photos = picturesArray(PICTURE_COUNT);
+var photosTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+var renderPicture = function () {
+  var pictureElement = photosTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = photos.url;
+  pictureElement.querySelector('.picture__comments').textContent = photos.comments;
+  pictureElement.querySelector('.picture__likes').textContent = photos.likes;
+
+  return pictureElement;
+};
+
+var similarListElement = document.querySelector('.pictures');
+
+var createPicture = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < PICTURE_COUNT; i++) {
+    fragment.appendChild(renderPicture(photos[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+createPicture();
