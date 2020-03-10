cd62b379f2498b07bfc0d53862f12699d136ce3d
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-03-10 21:16:10 +0300

    Загрузка комментариев (поток мыслей)

--- js/preview.js
+++ js/preview.js
@@ -1,7 +1,9 @@
 'use strict';
 
 (function () {
-  var MAX_COMMENTS_COUNT = 5;
+  var COMMENTS_COUNT = 5;
+  var count = 0;
+
   var pageBody = document.querySelector('body');
   var pictures = document.querySelector('.pictures');
   var commentsList = document.querySelector('.social__comments');
@@ -21,21 +23,30 @@
     return commentElement;
   };
 
-
-  var getComments = function (comment) {
+  var getComments = function (comments) {
     commentsList.innerHTML = '';
     var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < comment.length; i++) {
-      fragment.appendChild(createNewComment(comment[i]));
+    for (var i = 0; i < comments.length; i++) {
+      fragment.appendChild(createNewComment(comments[i]));
     }
     commentsList.appendChild(fragment);
+  };
 
-    if (comment.length < MAX_COMMENTS_COUNT) {
+  var commentsArray = [];
+
+  var renderNewComments = function (comment) {
+    commentsArray = comment.slice(count, COMMENTS_COUNT + count);
+    count += COMMENTS_COUNT;
+
+    if (commentsArray > COMMENTS_COUNT) {
       commentsLoader.classList.add('hidden');
     } else {
       commentsLoader.classList.remove('hidden');
     }
+
+    getComments(commentsArray);
+    console.log(count);
+    console.log(commentsArray);
   };
 
   var renderBigPicture = function (photo) {
@@ -44,7 +55,7 @@
     bigPicture.querySelector('.social__comments').textContent = photo.comments;
     bigPicture.querySelector('.social__caption').textContent = photo.description;
     bigPicture.querySelector('.likes-count').textContent = photo.likes;
-    getComments(photo.comments);
+    renderNewComments(photo.comments);
   };
 
   pictures.addEventListener('click', function (evt) {
@@ -52,6 +63,8 @@
       var index = parseInt(evt.target.dataset.index, 10);
       bigPicture.classList.remove('hidden');
       document.body.classList.add('modal-open');
+      // count = 0;
+      // renderNewComments(window.data.photosArr);
 
       if (window.data.photosArr) {
         renderBigPicture(window.data.photosArr[index]);
@@ -83,12 +96,7 @@
 
   pageBody.addEventListener('keydown', onPopupEscPress);
 
-  commentsLoader.addEventListener('click', function (comment) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < MAX_COMMENTS_COUNT; i++) {
-      fragment.appendChild(createNewComment(comment[i]));
-    }
-    commentsList.appendChild(fragment);
+  commentsLoader.addEventListener('click', function () {
+    renderNewComments(window.data.photosArr.comments);
   });
 })();
